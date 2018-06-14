911f44a0d9f46249c006ce6b09bddee243fbf606
johnmorales990@gmail.com <johnmorales990@gmail.com>
2018-06-14 13:21:50 -0300

    final

--- config/auth.js
+++ config/auth.js
@@ -4,9 +4,9 @@
 module.exports = {
 
 	'facebookAuth' : {
-		'clientID' 		: '180068836015039', // your App ID
-		'clientSecret' 	: 'df26bb79969e62ab72822128b3ca2a14', // your App Secret
-		'callbackURL' 	: 'https://fortalk.herokuapp.com/auth/facebook/callback'
+		'clientID' 		: process.env.FACEBOOK_clientID, // your App ID
+		'clientSecret' 	: process.env.FACEBOOK_clientSecret, // your App Secret
+		'callbackURL' 	: process.env.FACEBOOK_callbackURL //looks like http://localhost/auth/facebook/callback
 	}
 
 };
