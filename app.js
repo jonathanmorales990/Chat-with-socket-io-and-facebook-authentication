579893b6238c76c183a9d037e4bf4b57960d5421
johnmorales990@gmail.com <johnmorales990@gmail.com>
2018-06-14 12:04:04 -0300

    Ajustado

--- app.js
+++ app.js
@@ -51,34 +51,34 @@ app.use(passport.session());
 var db = mongoose.connection;
 
 db.on('connecting', function() {
-    console.log('connecting to MongoDB...');
-  });
+  console.log('connecting to MongoDB...');
+});
 
 db.on('error', function(error) {
-    console.error('Error in MongoDb connection: ' + error);
-    mongoose.disconnect();
-  });
+  console.error('Error in MongoDb connection: ' + error);
+  mongoose.disconnect();
+});
 db.on('connected', function() {
-    console.log('MongoDB connected!');
-  });
+  console.log('MongoDB connected!');
+});
 db.once('open', function() {
-    console.log('MongoDB connection opened!');
-  });
+  console.log('MongoDB connection opened!');
+});
 db.on('reconnected', function () {
-    console.log('MongoDB reconnected!');
-  });
+  console.log('MongoDB reconnected!');
+});
 db.on('disconnected', function() {
-    console.log('MongoDB disconnected!');
-    mongoose.connect(configDB.url, {server:{auto_reconnect:true}});
-  });
+  console.log('MongoDB disconnected!');
+  mongoose.connect(configDB.url, {server:{auto_reconnect:true}});
+});
 
 mongoose.connect(configDB.url, {server:{auto_reconnect:true}});
 
 
 process.on('SIGINT', function(params) {
-        console.log('Tchau!');
-        db.close();
-        process.exit();
+  console.log('Tchau!');
+  db.close();
+  process.exit();
 });
 
 
--- config/socketio.js
+++ config/socketio.js
@@ -23,10 +23,36 @@ io.sockets.on( "connection", function( socket ){
         if(socket.handshake.session.socketID){
             
             User.findById( socket.handshake.session.passport.user, 'socketId facebook.name facebook.email facebook.photo local.nomecompleto local.nome local.sobrenome local.email local.photo',
+                function(err, model) {
+
+                    if(err){
+                         
+                        socket.emit("error", 'Authentication error');
+
+                    }
+                    if(!model){
+
+                        socket.emit("error", 'Authentication error');
+                    
+                    }
+
+                    if(model){
+
+                        socket.join( socket.handshake.session.passport.user );
+                        socket.broadcast.emit('newUser', model);
+                        socket.emit('myId', model._id);
+                    }
+
+            });
+
+        } else {
+            User.findByIdAndUpdate(socket.handshake.session.passport.user,
+                { $set: { 'socketId': socket.id} },{safe: true, upsert: true , new:true, 
+                fields:'socketId facebook.name facebook.email facebook.photo local.nomecompleto local.nome local.sobrenome local.email local.photo'},
                     function(err, model) {
 
                         if(err){
-                         
+                                 
                             socket.emit("error", 'Authentication error');
 
                         }
@@ -37,88 +63,56 @@ io.sockets.on( "connection", function( socket ){
 
                         if(model){
 
-                            socket.join( socket.handshake.session.passport.user );
+                            socket.handshake.session.socketID = socket.id;
+                            socket.handshake.session.save();
+
+                            socket.join(socket.handshake.session.passport.user);
+
                             socket.broadcast.emit('newUser', model);
+
                             socket.emit('myId', model._id);
 
                         }
-
-                    });
-
-                } else {
-           
-
-                            User.findByIdAndUpdate(
-                            socket.handshake.session.passport.user,
-                            { $set: { 'socketId': socket.id} },{safe: true, upsert: true , new:true, 
-                            fields:'socketId facebook.name facebook.email facebook.photo local.nomecompleto local.nome local.sobrenome local.email local.photo'},
-                            function(err, model) {
-
-                                if(err){
-                                 
-                                    socket.emit("error", 'Authentication error');
-
-                                }
-                                if(!model){
-
-                                    socket.emit("error", 'Authentication error');
-                                }
-
-                                if(model){
-
-                                    socket.handshake.session.socketID = socket.id;
-                                    socket.handshake.session.save();
-
-                                    socket.join(socket.handshake.session.passport.user);
-
-                                    socket.broadcast.emit('newUser', model);
-
-                                    socket.emit('myId', model._id);
-                                }
                                    
-                            });
+                    });
                        
-                        } 
+        } 
                    
     } else { socket.emit("error", 'Authentication error'); }
 
-   socket.on('mensagem', function (data) {
+    socket.on('mensagem', function (data) {
 
             ////data to string
          
-			var newMensagem             = new Mensagem();
+        var newMensagem             = new Mensagem();
 
-            newMensagem.de              = striptags(data.de);
-            newMensagem.para            = striptags(data.para);
-            newMensagem.mensagemEnviada = striptags(data.mensagemEnviada);
+        newMensagem.de              = striptags(data.de);
+        newMensagem.para            = striptags(data.para);
+        newMensagem.mensagemEnviada = striptags(data.mensagemEnviada);
 
-            newMensagem.save(function(err, mensagem) {
-                if(err){
+        newMensagem.save(function(err, mensagem) {
+            if(err){
 
-                    socket.emit('erroMensagem', mensagem);
+                socket.emit('erroMensagem', mensagem);
 
-                }
+            }
 
-                if(mensagem){
-                    
+            if(mensagem){
 
-                    socket.emit('confirmaMensagem', data);
-                    socket.broadcast.to(data.para).emit('novaMensagem', mensagem);
+                socket.emit('confirmaMensagem', data);
+                socket.broadcast.to(data.para).emit('novaMensagem', mensagem);
 
-                }
+            }
 
-			
         });
 
     });
    
    socket.on('requestMensagem', function (data) {
 
-            Mensagem.find( { $or: [ { de: socket.handshake.session.passport.user , para:data.id }, 
-                                    { de: data.id, para:socket.handshake.session.passport.user  },   
-                                    
-                                    ]
-                             },  function(err, mensagem){
+        Mensagem.find( { $or: [ { de: socket.handshake.session.passport.user , para:data.id }, 
+            { de: data.id, para:socket.handshake.session.passport.user  },]
+            },  function(err, mensagem){
                                 
                 if (err){
                  
@@ -131,31 +125,31 @@ io.sockets.on( "connection", function( socket ){
                    socket.emit('requestedMensagem', data);
                    
                 }else{
-                        data.mensagem = mensagem;
+                    data.mensagem = mensagem;
 
-                        if( mensagem[mensagem.length-1].readed === false && mensagem[mensagem.length-1].de !== socket.handshake.session.passport.user ){
+                    if( mensagem[mensagem.length-1].readed === false && mensagem[mensagem.length-1].de !== socket.handshake.session.passport.user ){
 
-                            data.removenotification = true;
+                        data.removenotification = true;
 
-                        }
+                    }
 
-                        Mensagem.update({ de: data.id, para:socket.handshake.session.passport.user },
-                            { readed: true } ,{multi: true} ,function(err, mensagem){
+                    Mensagem.update({ de: data.id, para:socket.handshake.session.passport.user },
+                        { readed: true } ,{multi: true} ,function(err, mensagem){
 
-                        if(err){
+                    if(err){
 
-                            socket.emit('requestedMensagemErro', data);
+                        socket.emit('requestedMensagemErro', data);
 
-                        }
-                        if(mensagem){
+                    }
+                    if(mensagem){
 
-                            socket.emit('requestedMensagem', data);
+                        socket.emit('requestedMensagem', data);
 
-                        }
+                    }
                         
                         
                         });
-                     }          
+                }          
         
         });
 
