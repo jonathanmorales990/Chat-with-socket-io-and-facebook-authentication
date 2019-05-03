ed2b6b78467cf0b1ccd1d36a46b245a29a21c8f1
Jonathan Morales <johnmorales990@gmail.com>
2019-05-03 15:02:23 -0300

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -4,25 +4,21 @@ var passport = require('passport');
 
 
 router.get('/', jaTaLogado, function(req, res, next) {
-  return res.render('login.ejs',  { message: req.flash('loginMessage'),title: 'Login' });
+	return res.render('login.ejs',  { message: req.flash('loginMessage'),title: 'Login' });
 });
 
 router.post('/',jaTaLogado, passport.authenticate('local-login', {
-			successRedirect : '/', // redirect to the secure profile section
-			failureRedirect : '/login', // redirect back to the signup page if there is an error
-			failureFlash : true // allow flash messages
-		}));
+	successRedirect : '/', // redirect to the secure profile section
+	failureRedirect : '/login', // redirect back to the signup page if there is an error
+	failureFlash : true // allow flash messages
+}));
 
 function jaTaLogado(req, res, next) {
-    if(req.isAuthenticated()){
-      //if user is looged in, req.isAuthenticated() will return true 
-      return res.redirect(303,"/");
-      
-    } else{
-
-      return next();
-    
-    }
+	if (req.isAuthenticated()) {//if user is looged in, req.isAuthenticated() will return true 
+		return res.redirect(303,"/");
+	} else {
+		return next();
+	}
 }
 
 module.exports = router;
