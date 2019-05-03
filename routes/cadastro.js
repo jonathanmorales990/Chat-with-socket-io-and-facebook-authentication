9d5885e35e4a9fcd61e56733cd2b7cf8b31c460d
Jonathan Morales <johnmorales990@gmail.com>
2019-05-03 15:03:00 -0300

    Update cadastro.js

--- routes/cadastro.js
+++ routes/cadastro.js
@@ -4,22 +4,18 @@ var passport = require('passport');
 
 
 router.get('/', function(req, res, next) {
-
 	return res.render('cadastro.ejs',  {	
 		messageErro: req.flash('signupMessageError'), 
 		messageSucesso: req.flash('signupMessageSucess'), 
 		title: 'Cadastro' 
 	});
-  
 });
 
 router.post('/', passport.authenticate('local-signup', {
-
 	successRedirect : '/cadastro', 
 	failureRedirect : '/cadastro', // redirect back to the signup page if there is an error
 	failureFlash : true, // allow flash messages
 	session:false
-
 }));
 
 
