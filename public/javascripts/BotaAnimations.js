6361e17d1e61273fbd2c8616df9d3379de23b849
Jonathan Morales <johnmorales990@gmail.com>
2019-05-03 15:46:47 -0300

    Update BotaAnimations.js

--- public/javascripts/BotaAnimations.js
+++ public/javascripts/BotaAnimations.js
@@ -1,33 +1,18 @@
 $(document).ready(function(){
-
-		    $(document).on({
-
+		$(document).on({
 			    mouseenter: function () {
 			       $(this).stop().animate({ backgroundColor: "#216955" }, 300);},
 			    mouseleave: function () {
 			        $(this).stop().animate({ backgroundColor: "#004a35" }, 300);},
-			    
 			}, ".botaoEnviaMenssagem");
-
-		 });
-		 
-		 
-	   	$(document).on('click', '.iconeFechar',function(){
-			
+		});
+	   	$(document).on('click', '.iconeFechar',function(){	
 			$(this).parent().parent().remove();
-
 			var id = $(this).parent().prev().val();
-
-			/*alert(id);*/
-			$('.usuarioNomeMenuLateal').each(function(){
-
-				
-				if($(this).prev().val() === id){
-
-					$(this).addClass('AtivarChat');
-				
-				}
-
-			});
 			
+			$('.usuarioNomeMenuLateal').each(function(){
+				if($(this).prev().val() === id){
+					$(this).addClass('AtivarChat');
+				}
+			});
 		});
