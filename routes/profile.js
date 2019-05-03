ab29a3f8a9623a34484974730b11ec1a193c6972
Jonathan Morales <johnmorales990@gmail.com>
2019-05-03 15:08:43 -0300

    Update profile.js

--- routes/profile.js
+++ routes/profile.js
@@ -9,20 +9,11 @@ router.get('/', isLoggedIn , function(req, res, next) {
 
 	User.find( { _id:{ $ne:req.user._id } }, function (err, model) {
 		
-		if(err){
-			
-			return res.redirect(303,"/login");
+		if(err) return res.redirect(303,"/login");
 		
-		}
-
-		if(!model){
-
-			return res.render('profile.ejs', {user : req.user});
-			 
-		}
+		if(!model) return res.render('profile.ejs', {user : req.user});
  
 		if(model){
-
 			userId = req.user._id.toString();
 			Mensagem.aggregate([
 			        // Matching pipeline, similar to find
@@ -58,41 +49,21 @@ router.get('/', isLoggedIn , function(req, res, next) {
 			                "de": "$_id",
 			                "mensagemEnviada": 1,
 			                "data": 1
-			            }
-			}], function(err, mensagem){
-			                                    
-			                    if (err){
-
-			                        
-     								 return res.redirect(303,"/login");
-			                   
-			                    }
-
-			                    if(mensagem){
-			                        
-			                        return res.render('profile.ejs', {user : req.user, allUsers : model, notification: mensagem});
-
-			                    }          
-			                
-			            });   
-			
-			
+				}
+			}], function(err, mensagem){                     
+			                    if (err) return res.redirect(303,"/login");
+			               
+			                    if(mensagem) return res.render('profile.ejs', {user : req.user, allUsers : model, notification: mensagem});                  
+			});   
 		}
-		
-
-	
 	});  
 }); 
 
 function isLoggedIn(req, res, next) {
-	if(req.isAuthenticated()){
-	//if user is looged in, req.isAuthenticated() will return true 
+	if (req.isAuthenticated()) { //if user is looged in, req.isAuthenticated() will return true 
 		return next();
-      
-	}else{
-
+	} else {
 		return res.redirect(303,"/login");
-    
 	}
 }
 
