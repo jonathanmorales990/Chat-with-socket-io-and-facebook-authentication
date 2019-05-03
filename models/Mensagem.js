72a0dd1913bf2d803ef7a58e49c6a718806d8359
Jonathan Morales <johnmorales990@gmail.com>
2019-05-03 15:44:00 -0300

    Update Mensagem.js

--- models/Mensagem.js
+++ models/Mensagem.js
@@ -2,17 +2,14 @@
 var mongoose = require('mongoose');
 
 var userSchema = mongoose.Schema({
-
-    
+	
 	data: { type: Date, default: Date.now },
 	de: String,
 	para: String,
 	mensagemEnviada: String,
 	readed: { type: Boolean, default: false }
-
-
+	
 },{collection: 'Mensagem'});
-
 
 // create the model for users and expose it to our app
 module.exports = mongoose.model('Mensagem', userSchema);
