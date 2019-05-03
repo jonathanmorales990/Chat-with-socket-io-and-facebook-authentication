b31217b8c1964ebb43cfa431c516bd61c1ec63d4
Jonathan Morales <johnmorales990@gmail.com>
2019-05-03 15:00:38 -0300

    Update sair.js

--- routes/sair.js
+++ routes/sair.js
@@ -5,32 +5,26 @@ var passport = require('passport');
 var User = require('../models/User');
 
 router.get('/facebook',isLoggedIn, function(req, res) { 
-		var user = req.user;
-		req.user = null;
-		req.logout();
-		res.redirect('/login');
-		
-	});
+	var user = req.user;
+	req.user = null;
+	req.logout();
+	res.redirect('/login');
+});
 
 router.get('/local',isLoggedIn, function(req, res) {
-		var user = req.user;
-		req.user = null;
-		req.logout();
-		res.redirect('/login');
-					
-	});
+	var user = req.user;
+	req.user = null;
+	req.logout();
+	res.redirect('/login');					
+});
 
 
 function isLoggedIn(req, res, next) {
-	if(req.isAuthenticated()){
-      //if user is looged in, req.isAuthenticated() will return true 
-      return next();
-      
-    } else{
-
-      return res.redirect(303,"/login");
-    
-    }
+	if (req.isAuthenticated()) {//if user is looged in, req.isAuthenticated() will return true 
+		return next();
+	} else{
+		return res.redirect(303,"/login");
+	}
 }
 
 module.exports = router;
