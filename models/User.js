7c0611abb03860682393aea979d3d0e9bb8c2eed
Jonathan Morales <johnmorales990@gmail.com>
2019-05-03 15:44:24 -0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,9 +2,7 @@
 var mongoose = require('mongoose');
 var bcrypt   = require('bcrypt-nodejs');
 // define the schema for our user model
-var userSchema = mongoose.Schema({
-
-    
+var userSchema = mongoose.Schema({   
     facebook: {
         id: String,
         token: String,
