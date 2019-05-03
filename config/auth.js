e3431ad7d38e5ebf3a58b1f380e6071b8c1afddd
Jonathan Morales <johnmorales990@gmail.com>
2019-05-03 15:10:01 -0300

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,9 +1,7 @@
 module.exports = {
-
 	'facebookAuth' : {
 		'clientID': process.env.FACEBOOK_clientID, // your App ID
 		'clientSecret': process.env.FACEBOOK_clientSecret, // your App Secret
 		'callbackURL': process.env.FACEBOOK_callbackURL //looks like http://localhost/auth/facebook/callback
 	}
-
 };
